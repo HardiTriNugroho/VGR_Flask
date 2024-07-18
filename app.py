161f2810570afcfecd5b6127ac89da3beca1084e
sektelembahberacun <146374025+HardiTriNugroho@users.noreply.github.com>
2024-07-19 00:37:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
 import tensorflow as tf
-import soundfile as sf
-import os
 import librosa
-from PreProccesing import preprocess_audio  # Import your preprocessing functions
+import os
+from PreProccesing import preprocess_audio  # Ensure this module exists
 
 app = Flask(__name__)
 
 # Load your model
 model = tf.keras.models.load_model('model/VGR_LIB_augmented_v2_EP30.h5')
 
-# Define MFCC extraction and padding functions
 def extract_mfcc(audio, sample_rate=16000, n_mfcc=13):
     mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=n_mfcc)
     return mfcc
@@ -43,16 +41,14 @@ def predict():
     file_path = 'temp.wav'
     file.save(file_path)
 
-    # Preprocess the audio file
     audio, sample_rate = preprocess_audio(file_path)
     if audio is None:
         return jsonify({'error': 'Error in preprocessing audio file'}), 500
 
     mfcc = extract_mfcc(audio, sample_rate)
     padded_mfcc = pad_or_truncate(mfcc, max_length=300)
-    processed_audio = np.expand_dims(padded_mfcc, axis=0)  # Add batch dimension
+    processed_audio = np.expand_dims(padded_mfcc, axis=0)
 
-    # Make prediction
     prediction = model.predict(processed_audio)
     gender = 'Male' if prediction[0][0] > 0.5 else 'Female'
     accuracy = float(prediction[0][0]) if gender == 'Male' else 1 - float(prediction[0][0])
